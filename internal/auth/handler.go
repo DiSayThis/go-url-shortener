42f034fa840bb283b60581246603b64ee13e65a0
Dmitriy <disaythis@gmail.com>
2026-08-04 17:01:39 +0300

    feat(auth): улучшение обработки запросов и валидации данных для аутентификации и регистрации

--- internal/auth/handler.go
+++ internal/auth/handler.go
@@ -1,12 +1,11 @@
 package auth
 
 import (
-	"encoding/json"
 	"fmt"
 	"go-api/configs"
-	"go-api/pkg/res"
+	"go-api/pkg/request"
+	"go-api/pkg/response"
 	"net/http"
-	"regexp"
 )
 
 type AuthHandlerDeps struct {
@@ -24,32 +23,25 @@ func NewAuthHandler(router *http.ServeMux, deps AuthHandlerDeps) {
 }
 
 func (handler *AuthHandler) login(w http.ResponseWriter, req *http.Request) {
-	var payload LoginRequest
-	err := json.NewDecoder(req.Body).Decode(&payload)
+	payload, err := request.HandleBody[LoginRequest](w, req)
 	if err != nil {
-		res.JsonResponse(w, map[string]string{"error": "Invalid request body: " + err.Error()}, http.StatusBadRequest)
 		return
 	}
-	emailRegex, _ := regexp.Compile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
-	if payload.Email == "" {
-		res.JsonResponse(w, map[string]string{"error": "Email is required"}, http.StatusBadRequest)
-		return
-	}
-	if !emailRegex.MatchString(payload.Email) {
-		res.JsonResponse(w, map[string]string{"error": "Invalid email format"}, http.StatusBadRequest)
-		return
-	}
-	if payload.Password == "" {
-		res.JsonResponse(w, map[string]string{"error": "Password is required"}, http.StatusBadRequest)
-		return
-	}
-	fmt.Println("login", payload.Email, payload.Password)
+	fmt.Println("login", payload)
 	data := LoginResponse{
 		Token: handler.Config.Auth.Secret,
 	}
-	res.JsonResponse(w, data, http.StatusOK)
+	response.JsonResponse(w, data, http.StatusOK)
 }
 
 func (handler *AuthHandler) register(w http.ResponseWriter, req *http.Request) {
-	fmt.Println("register")
+	payload, err := request.HandleBody[RegisterRequest](w, req)
+	if err != nil {
+		return
+	}
+	fmt.Println("register", payload)
+	data := RegisterResponse{
+		Token: handler.Config.Auth.Secret,
+	}
+	response.JsonResponse(w, data, http.StatusCreated)
 }
---
+++ pkg/request/validate.go
@@ -0,0 +1,11 @@
+package request
+
+import (
+	"github.com/go-playground/validator/v10"
+)
+
+func IsValid[T any](payload T) error {
+	validator := validator.New()
+	err := validator.Struct(payload)
+	return err
+}
