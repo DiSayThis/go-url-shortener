6c28fadbe16978d4fefe7191b8cef04a57f95907
Dmitriy <disaythis@gmail.com>
2026-08-24 18:00:48 +0300

    chore: начал middleware чтения токена

--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"net/http"
+	"strings"
 
 	"go-api/internal/auth"
 )
@@ -20,4 +21,32 @@ func RequireAuth(next AuthenticatedHandlerFunc) http.Handler {
 		}
 		next(w, req)
 	})
+}
+
+func FindToken(tokenService *auth.JWTAccessTokenService) func(http.Handler) http.Handler {
+
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(
+			func(w http.ResponseWriter, r *http.Request) {
+				bearerToken := strings.TrimSpace(r.Header.Get("Authorization"))
+				if bearerToken == "" {
+					next.ServeHTTP(w, r)
+					return
+				}
+				parts := strings.Split(bearerToken, " ")
+				rawToken := parts[1]
+				if rawToken == "" {
+					next.ServeHTTP(w, r)
+					return
+				}
+				principal, err := tokenService.Parse(rawToken)
+				if err == nil {
+					next.ServeHTTP(w, r)
+					return
+				}
+
+				auth.WithPrincipal(r.Context(), principal)
+				next.ServeHTTP(w, r)
+			})
+	}
 }
