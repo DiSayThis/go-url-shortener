7fb3da9c89e1bea58afc007e72265add3df66d7e
Dmitriy <disaythis@gmail.com>
2026-08-28 15:41:06 +0300

    fix(request): отклонять неизвестные поля и лишний JSON

--- pkg/request/decode.go
+++ pkg/request/decode.go
@@ -2,15 +2,30 @@ package request
 
 import (
 	"encoding/json"
+	"errors"
 	"io"
 )
 
 func Decode[T any](body io.ReadCloser) (T, error) {
 	var payload T
 
-	err := json.NewDecoder(body).Decode(&payload)
-	if err != nil {
+	decoder := json.NewDecoder(body)
+	decoder.DisallowUnknownFields()
+
+	if err := decoder.Decode(&payload); err != nil {
 		return payload, err
 	}
+
+	var extra any
+	if err := decoder.Decode(&extra); !errors.Is(err, io.EOF) {
+		if err == nil {
+			return payload, errors.New(
+				"request body must contain one JSON value",
+			)
+		}
+
+		return payload, err
+	}
+
 	return payload, nil
 }
